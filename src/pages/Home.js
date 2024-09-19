0e306e6720c81882d1e2c2ed0e5b60659d1ba7c4
Marco de Castro <castromar189@gmail.com>
2024-09-19 16:57:47 -0300

    ,

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-function Home() {
-  return (
-    <div>
-      <h1>Bem-vindo à Página Inicial</h1>
-      <nav>
-        <ul>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/register">Registro</Link></li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
-
-export default Home;
+import redusida from '../images/redusida.jpg';
+import '../styles/Home.css';
+    
+    const Home = () => {
+      const token = localStorage.getItem('token');
+      const isLoggedIn = !!token;  
+      const userName = JSON.parse(localStorage.getItem('userData'))?.nome || ''; // Corrigido para acessar userData corretamente
+    
+      return (
+        <div className="logged-in-home-page">
+          <header className="home-header">
+            <h1>Bem-vindo{isLoggedIn ? `, ${userName}` : ''}!</h1>
+            <p>À Comemorações Maçônicas para Chancelaria.</p>
+          </header>
+    
+          <section className="home-content">
+            <p>Comemorações maçônicas para chancelaria, traz além de comemorações diárias, também oferece ao chanceler um controle de presenças de membros e página exclusiva com comemorações diárias e comemorações semanais de membros e efemérides. </p>
+    
+            <div className="home-image-container">
+              <img src={redusida} alt="Comemorações Maçônicas" className="home-image" />
+            </div>
+    
+            <div className="home-links">
+              <Link to="/login" className="home-link">Login</Link>
+              <Link to="/register" className="home-link">Registrar</Link>
+            </div>
+          </section>
+    
+          <footer id="footer">
+            <div>
+              <p>&copy; <span id="year">{new Date().getFullYear()}</span> <a href="https://www.facebook.com/notemasterRJ/" id="author" rel="noopener noreferrer" target="_blank">notemaster</a>. Todos os direitos reservados.</p>
+              <p>Desenvolvido por <a href="https://www.linkedin.com/in/marco-de-castro/" id="authorLink" rel="noopener noreferrer" target="_blank">Marco de Castro</a></p>
+            </div>
+          </footer>
+        </div>
+      );
+    }
+    
+    export default Home;
